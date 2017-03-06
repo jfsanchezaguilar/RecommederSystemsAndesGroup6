96ac151be8a3da21bacbd5265465f1737a4167d8
jsancheza <jfsanchezaguilar@gmail.com>
2017-03-06 09:15:17 -0500

    change to full paths

--- BE/API/RecommendApi/file_reader.py
+++ BE/API/RecommendApi/file_reader.py
@@ -1,25 +1,25 @@
 def read_file(algorithm, similarity, training):
     if algorithm == "Jaccard" and similarity == "Item":
-        with open("../../ItemBased/rj"+str(training)+".json", 'r') as content_file:
+        with open("/home/ubuntu/ItemBased/rj"+str(training)+".json", 'r') as content_file:
             content = content_file.read()
         return content
     elif algorithm == "Cosine" and similarity == "Item":
-        with open("../../ItemBased/rc"+str(training)+".json", 'r') as content_file:
+        with open("/home/ubuntu/ItemBased/rc"+str(training)+".json", 'r') as content_file:
             content = content_file.read()
         return content
     elif algorithm == "Pearson" and similarity == "Item":
-        with open("../../ItemBased/rp"+str(training)+".json", 'r') as content_file:
+        with open("/home/ubuntu/ItemBased/rp"+str(training)+".json", 'r') as content_file:
             content = content_file.read()
         return content
     if algorithm == "Jaccard" and similarity == "User":
-        with open("../../UserBased/rj"+str(training)+".json", 'r') as content_file:
+        with open("/home/ubuntu/UserBased/rj"+str(training)+".json", 'r') as content_file:
             content = content_file.read()
         return content
     elif algorithm == "Cosine" and similarity == "User":
-        with open("../../UserBased/rc"+str(training)+".json", 'r') as content_file:
+        with open("/home/ubuntu/UserBased/rc"+str(training)+".json", 'r') as content_file:
             content = content_file.read()
         return content
     elif algorithm == "Pearson" and similarity == "User":
-        with open("../../UserBased/rp"+str(training)+".json", 'r') as content_file:
+        with open("/home/ubuntu/UserBased/rp"+str(training)+".json", 'r') as content_file:
             content = content_file.read()
         return content
