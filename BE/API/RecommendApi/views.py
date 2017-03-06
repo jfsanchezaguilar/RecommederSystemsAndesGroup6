cd863730885770ca4b50e8dd17b739d14f68c323
jsancheza <jfsanchezaguilar@gmail.com>
2017-03-06 11:32:32 -0500

    fixed views

--- BE/API/RecommendApi/views.py
+++ BE/API/RecommendApi/views.py
@@ -1,13 +1,30 @@
 from rest_framework import generics
 from rest_framework import status
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from django.contrib.auth.models import User, Group
 from RecommendApi.file_reader import read_file
 from RecommendApi.file_updater import update_book_user_file, update_book_file, update_book_raiting_file
 from RecommendApi.models import RecommendTask, BookUser, Book, BookRaiting
 from RecommendApi.serializers import RecommendTaskSerializer, BookUserSerializer, \
-    BookSerializer, BookRaitingSerializer
+    BookSerializer, BookRaitingSerializer, UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
 
 
 class RecommendTaskDetails(generics.ListAPIView):
