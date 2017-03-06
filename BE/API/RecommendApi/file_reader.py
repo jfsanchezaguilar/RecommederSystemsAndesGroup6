f9c7a156d92e6e7be5372731f8d6fbe381020f1c
jsancheza <jfsanchezaguilar@gmail.com>
2017-03-06 10:00:22 -0500

    change to os path join

--- BE/API/RecommendApi/file_reader.py
+++ BE/API/RecommendApi/file_reader.py
@@ -1,25 +1,34 @@
+import os
+
+
 def read_file(algorithm, similarity, training):
     if algorithm == "Jaccard" and similarity == "Item":
-        with open("/home/ubuntu/ItemBased/rj"+str(training)+".json", 'r') as content_file:
+        path = os.path.join(os.path.expanduser('~'), 'ItemBased', "rj" + str(training) + ".json")
+        print(path)
+        with open(path, 'r') as content_file:
             content = content_file.read()
         return content
     elif algorithm == "Cosine" and similarity == "Item":
-        with open("/home/ubuntu/ItemBased/rc"+str(training)+".json", 'r') as content_file:
+        path = os.path.join(os.path.expanduser('~'), 'ItemBased', "rc" + str(training) + ".json")
+        print(path)
+        with open(path, 'r') as content_file:
             content = content_file.read()
         return content
     elif algorithm == "Pearson" and similarity == "Item":
-        with open("/home/ubuntu/ItemBased/rp"+str(training)+".json", 'r') as content_file:
+        path = os.path.join(os.path.expanduser('~'), 'ItemBased', "rp" + str(training) + ".json")
+        print(path)
+        with open(path, 'r') as content_file:
             content = content_file.read()
         return content
     if algorithm == "Jaccard" and similarity == "User":
-        with open("/home/ubuntu/UserBased/rj"+str(training)+".json", 'r') as content_file:
+        with open("/home/ubuntu/UserBased/rj" + str(training) + ".json", 'r') as content_file:
             content = content_file.read()
         return content
     elif algorithm == "Cosine" and similarity == "User":
-        with open("/home/ubuntu/UserBased/rc"+str(training)+".json", 'r') as content_file:
+        with open("/home/ubuntu/UserBased/rc" + str(training) + ".json", 'r') as content_file:
             content = content_file.read()
         return content
     elif algorithm == "Pearson" and similarity == "User":
-        with open("/home/ubuntu/UserBased/rp"+str(training)+".json", 'r') as content_file:
+        with open("/home/ubuntu/UserBased/rp" + str(training) + ".json", 'r') as content_file:
             content = content_file.read()
         return content
