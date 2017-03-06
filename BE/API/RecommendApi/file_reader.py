2f12d322b513954826c0b1cd2058f28aed7c9988
jsancheza <jfsanchezaguilar@gmail.com>
2017-03-06 09:03:08 -0500

    fixed paths

--- BE/API/RecommendApi/file_reader.py
+++ BE/API/RecommendApi/file_reader.py
@@ -1,25 +1,25 @@
 def read_file(algorithm, similarity, training):
     if algorithm == "Jaccard" and similarity == "Item":
-        with open("rj"+str(training)+".json", 'r') as content_file:
+        with open("../../../ItemBased/rj"+str(training)+".json", 'r') as content_file:
             content = content_file.read()
         return content
     elif algorithm == "Cosine" and similarity == "Item":
-        with open("rc"+str(training)+".json", 'r') as content_file:
+        with open("../../../ItemBased/rc"+str(training)+".json", 'r') as content_file:
             content = content_file.read()
         return content
     elif algorithm == "Pearson" and similarity == "Item":
-        with open("rp"+str(training)+".json", 'r') as content_file:
+        with open("../../../ItemBased/rp"+str(training)+".json", 'r') as content_file:
             content = content_file.read()
         return content
     if algorithm == "Jaccard" and similarity == "User":
-        with open("rj"+str(training)+".json", 'r') as content_file:
+        with open("../../../UserBased/rj"+str(training)+".json", 'r') as content_file:
             content = content_file.read()
         return content
     elif algorithm == "Cosine" and similarity == "User":
-        with open("rc"+str(training)+".json", 'r') as content_file:
+        with open("../../../UserBased/rc"+str(training)+".json", 'r') as content_file:
             content = content_file.read()
         return content
     elif algorithm == "Pearson" and similarity == "User":
-        with open("rp"+str(training)+".json", 'r') as content_file:
+        with open("../../../UserBased/rp"+str(training)+".json", 'r') as content_file:
             content = content_file.read()
         return content
