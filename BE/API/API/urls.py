8f0838d723f999358548a844d6c985745c4f6ed4
jsancheza <jfsanchezaguilar@gmail.com>
2017-03-06 11:29:32 -0500

    added back urls

--- BE/API/API/urls.py
+++ BE/API/API/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from rest_framework import routers
+
 from RecommendApi import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
 urlpatterns = [
+    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'bookusers/', views.book_user_list),
     url(r'books/', views.book_list),
